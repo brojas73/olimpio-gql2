e2d93c383d8c951b17dbff920bbcb083ccf62d6c
Braulio Rojas <brojas73@hotmail.com>
2023-04-03 10:02:41 -0500

    Commit

--- server-rest/db_updates.sql
+++ server-rest/db_updates.sql
@@ -1,3 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2023 at 05:01 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `olimpio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forma_pago`
+--
+
+CREATE TABLE `forma_pago` (
+  `id_forma_pago` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `forma_pago`
+--
+
+INSERT INTO `forma_pago` (`id_forma_pago`, `nombre`, `estado`) VALUES
+(1, 'Efectivo', 1),
+(2, 'Tarjeta', 1),
+(3, 'Transferencia', 1),
+(4, 'Múltiples Formas', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `forma_pago`
+--
+ALTER TABLE `forma_pago`
+  ADD PRIMARY KEY (`id_forma_pago`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `forma_pago`
+--
+ALTER TABLE `forma_pago`
+  MODIFY `id_forma_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 -- phpMyAdmin SQL Dump
 -- version 5.2.0
